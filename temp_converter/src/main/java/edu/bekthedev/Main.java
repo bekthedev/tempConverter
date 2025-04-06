50367b493872cb79c28296e424cfaa87fa81e39a
Bek Johansson <beksaylor@gmail.com>
2025-04-06 07:34:01 -0500

    finished code

---
+++ temp_converter/src/main/java/edu/bekthedev/Main.java
@@ -0,0 +1,33 @@
+package edu.bekthedev;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       
+        Scanner scanner = new Scanner(System.in);
+
+
+        //Variables
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.println("Enter the temperature: ");
+        temp = scanner.nextDouble();
+
+        System.out.println("Convert to Celsius or Fahreheit? (C or F)");
+        unit = scanner.next().toUpperCase();
+
+        //ternary operator (condition) ? true : false
+
+       newTemp =  (unit.equals("C")) ? (temp - 32) * 5/9 : (temp * 9/5) + 32;
+
+        System.out.printf("%.1f° %s",newTemp, unit);
+
+        scanner.close();
+
+
+    }
+}
